34f7e822853c61e3cf246714708185cc2770b8ff
Antonio Tripodi <Tony133@users.noreply.github.com>
2023-10-11 11:37:05 +0200

    chore(): update postgres-core.module.ts

--- lib/postgres-core.module.ts
+++ lib/postgres-core.module.ts
@@ -27,7 +27,7 @@ export class PostgresCoreModule implements OnApplicationShutdown {
     private readonly moduleRef: ModuleRef,
   ) {}
 
-  static forRoot(
+  public static forRoot(
     options: PostgresModuleOptions,
     connection?: string,
   ): DynamicModule {
